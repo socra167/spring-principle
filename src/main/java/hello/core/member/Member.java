99bf6261002e38a48d33fa127eac65e400e4b56e
socra167 <socra167@gmail.com>
2024-12-30 00:26:01 +0900

    :sparkles: feat: 회원 도메인 기능 구현

---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,41 @@
+package hello.core.member;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Member setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Member setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public Member setGrade(Grade grade) {
+        this.grade = grade;
+        return this;
+    }
+}
