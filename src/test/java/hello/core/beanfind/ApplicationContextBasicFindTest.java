7b221dfb2f9e7ee85d80db86eb0d8f0a87e4cdcf
socra167 <socra167@gmail.com>
2025-01-06 12:13:21 +0900

    :rotating_light: test: 동일한 타입이 둘 이상인 빈 조회

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ApplicationContextBasicFindTest {
+class ApplicationContextBasicFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
