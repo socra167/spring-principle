9a9c136422100d33f436b81a4ba8aa2bcdd356b2
socra167 <socra167@gmail.com>
2025-01-08 11:07:59 +0900

    :rotating_light: test: 컴포넌트 스캔으로 등록된 빈 테스트

---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,20 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AutoAppConfigTest {
+
+    @Test
+    @DisplayName("컴포넌트 스캔으로 빈 등록")
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
