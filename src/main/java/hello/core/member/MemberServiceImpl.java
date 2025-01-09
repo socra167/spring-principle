babc3bdb7827088eadf648a74341900653da424b
socra167 <socra167@gmail.com>
2025-01-09 10:00:59 +0900

    :wrench: config: autowired

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -6,25 +6,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
 
-    @Autowired // ac.getBean(MemberRepository.class)
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+	@Autowired // ac.getBean(MemberRepository.class)
+	public MemberServiceImpl(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
-    @Override
-    public void join(Member member) {
-        memberRepository.save(member);
-    }
+	@Override
+	public void join(Member member) {
+		memberRepository.save(member);
+	}
 
-    @Override
-    public Member findMember(Long memberId) {
-        return memberRepository.findId(memberId);
-    }
+	@Override
+	public Member findMember(Long memberId) {
+		return memberRepository.findId(memberId);
+	}
 
-    // 테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
+	// 테스트 용도
+	public MemberRepository getMemberRepository() {
+		return memberRepository;
+	}
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -26,6 +26,7 @@ public class OrderServiceImpl implements OrderService {
         return this;
     }
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
