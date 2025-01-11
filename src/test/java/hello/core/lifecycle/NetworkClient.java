0cf4b5e0fb194bfd47a72f765fcf520afdca6bca
socra167 <socra167@gmail.com>
2025-01-11 21:25:45 +0900

    :rotating_light: test: ``@PostConstruct`, `@PreDestroy`로 빈 생명주기 콜백 적용

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
 	private String url;
@@ -24,12 +27,14 @@ public class NetworkClient {
 		System.out.println("call: " + url + " message = " + message);
 	}
 
+	@PostConstruct
 	public void init() {
 		System.out.println("NetworkClient.init");
 		connect();
 		call("초기화 연결 메시지");
 	}
 
+	@PreDestroy
 	public void close() {
 		System.out.println("NetworkClient.close");
 		disconnect();
