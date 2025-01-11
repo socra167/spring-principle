eda2ee8aea8d1ca441fcdf620e86199c08028aaa
socra167 <socra167@gmail.com>
2025-01-11 20:20:53 +0900

    :rotating_light: test: 조회한 빈이 모두 필요할 때, List, Map 활용

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,53 @@
+package hello.core.autowired;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class AllBeanTest {
+
+	@Test
+	@DisplayName("조회한 빈이 모두 필요할 때, List, Map 활용")
+	void findAllBean() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+		DiscountService discountService = ac.getBean(DiscountService.class);
+		Member member = new Member(1L, "userA", Grade.VIP);
+		int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+		assertThat(discountService).isInstanceOf(DiscountService.class);
+		assertThat(discountPrice).isEqualTo(1000);
+
+		int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+		assertThat(rateDiscountPrice).isEqualTo(2000);
+	}
+
+	static class DiscountService {
+		private final Map<String, DiscountPolicy> policyMap;
+		private final List<DiscountPolicy> policies;
+
+		@Autowired
+		public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+			this.policyMap = policyMap;
+			this.policies = policies;
+			System.out.println("policyMap = " + policyMap);
+			System.out.println("policyList = " + policies);
+		}
+
+		public int discount(Member member, int price, String discountCode) {
+			DiscountPolicy discountPolicy = policyMap.get(discountCode);
+			return discountPolicy.discount(member, price);
+		}
+	}
+}
