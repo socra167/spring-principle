897a9f223429f4d9a7c4aa5ea52f0338beebb05e
socra167 <socra167@gmail.com>
2025-01-12 20:10:34 +0900

    :rotating_light: test: `JSR-330` `Provider`로 프로토타입 빈 사용

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,13 +3,13 @@ package hello.core.scope;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 
 class SingletonWithPrototypeTest1 {
 
@@ -42,10 +42,10 @@ class SingletonWithPrototypeTest1 {
 	static class ClientBean {
 
 		@Autowired
-		private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+		private Provider<PrototypeBean> prototypeBeanProvider;
 
 		public int logic() {
-			PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+			PrototypeBean prototypeBean = prototypeBeanProvider.get();
 			prototypeBean.addCount();
 			return prototypeBean.getCount();
 		}
