174f2f882447f67d81cc208f2bffa5235a567550
socra167 <socra167@gmail.com>
2025-01-12 17:50:24 +0900

    :rotating_light: test: 프로토타입 빈 요청 테스트

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,44 @@
+package hello.core.scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+class PrototypeTest {
+
+	@Test
+	@DisplayName("display_name_value")
+	void prototypeBeanFind() {
+		// AnnotationConfig ... 에서 컴포넌트(PrototypeBean.class)를 넣어서 컨테이너에 등록
+		// 직접 넣으면 컴포넌트처럼 컨테이너에 등록된다.
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+		System.out.println("find prototypeBean1");
+		PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+		System.out.println("find prototypeBean2");
+		PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+		System.out.println("prototypeBean1 = " + prototypeBean1);
+		System.out.println("prototypeBean2 = " + prototypeBean2);
+
+		assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+	}
+
+	@Scope("prototype")
+	static class PrototypeBean {
+
+		@PostConstruct
+		public void init() {
+			System.out.println("SingletonBean.init");
+		}
+
+		@PreDestroy
+		public void destroy() {
+			System.out.println("SingletonBean.destroy");
+		}
+	}
+}
