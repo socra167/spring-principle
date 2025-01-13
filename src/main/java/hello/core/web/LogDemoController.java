60ff94d21b39a84a5270fc3f58c97ee68cdd5533
socra167 <socra167@gmail.com>
2025-01-13 20:51:25 +0900

    :wrench: fix: MyLogger에 `proxyMode`를 적용해 사용

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,7 +2,6 @@ package hello.core.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,14 +15,14 @@ import hello.core.common.MyLogger;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final ObjectProvider<MyLogger> myLoggerProvider;
+	private final MyLogger myLogger;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
 
-		MyLogger myLogger = myLoggerProvider.getObject();
+		System.out.println("myLogger = " + myLogger.getClass());
 		myLogger.setRequestURL(requestURL);
 		myLogger.log("controller test");
 
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -1,6 +1,5 @@
 package hello.core.web;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -11,10 +10,9 @@ import hello.core.common.MyLogger;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-	private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+	private final MyLogger myLogger;
 
 	public void logic(String id) {
-		MyLogger myLogger = myLoggerObjectProvider.getObject();
 		myLogger.log("service id = " + id);
 	}
 }
