22e3e8bd96c5fb5e611a93f94c7db65fd0e4c80b
socra167 <socra167@gmail.com>
2025-01-13 17:07:48 +0900

    :wrench: fix: `ObjectProvider`로 MyLogger 사용하도록 변경해 문제 해결

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -1,6 +1,7 @@
 package hello.core.web;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,20 +9,22 @@ import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
-@Component
+@Controller
 @RequiredArgsConstructor
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerProvider;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
-		myLogger.setRequestURL(requestURL);
 
+		MyLogger myLogger = myLoggerProvider.getObject();
+		myLogger.setRequestURL(requestURL);
 		myLogger.log("controller test");
+
 		logDemoService.logic("testId");
 		return "OK";
 	}
